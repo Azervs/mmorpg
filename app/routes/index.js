40a4265d579d5c518e9b5072751763e3809646d7
Rodrigo Luan <rlstrabalho@hotmail.com>
2018-08-28 16:37:30 -0300

    Add files via upload

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,10 @@
 module.exports = function(application){
 	application.get('/', function(req, res){
-		res.render('index');
+		application.app.controllers.index.index(application, req, res);
+	});
+
+	application.post('/autenticar', function(req, res){
+		//res.send('chegando aqui');
+		application.app.controllers.index.autenticar(application, req, res);
 	});
 }
