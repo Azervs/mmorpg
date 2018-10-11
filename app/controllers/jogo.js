40f305593dfc8ff2116c36a9b40e7f20cf679775
Rodrigo Luan <rlstrabalho@hotmail.com>
2018-10-11 02:22:12 -0300

    Add files via upload

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -41,7 +41,13 @@ module.exports.pergaminhos = function(application, req, res){
 		res.send('Usuario precisa fazer login');
 		return;
 	} 
-	res.render("pergaminhos", {validacao: {}});
+	
+	var JogoDAO = getJogoDAO(application);
+	var usuario = req.session.usuario;
+
+	JogoDAO.getAcoes(res, usuario);
+	
+	//res.render("pergaminhos", {validacao: {}});
 }
 
 module.exports.ordernar_acao_sudito = function(application, req, res){
@@ -63,9 +69,44 @@ module.exports.ordernar_acao_sudito = function(application, req, res){
 	var JogoDAO = getJogoDAO(application);
 
 	dadosForm.usuario = req.session.usuario;
+
+	dadosForm.acao_termina_em = getTempoTerminaEm(dadosForm.acao);
+
 	JogoDAO.acao(res ,dadosForm);
+
 	res.redirect('jogo?msg=B');
 }
+
+
+module.exports.revogar_acao = function(application, req, res){
+	var url_query = req.query;
+	var id_acao = url_query.id_acao;
+
+	var JogoDAO = getJogoDAO(application);
+	JogoDAO.revogarAcao(res, id_acao);
+}
+
+function getJogoDAO(application){
+	var connection = application.config.dbConnection;
+	return new application.app.models.JogoDAO(connection);
+}
+
+function getTempoTerminaEm(acao){
+
+	var tempo = null;
+
+	switch(parseInt(acao)){
+		case 1: tempo = 1 * 60 * 60000; break;
+		case 2: tempo = 2 * 60 * 60000; break;
+		case 3: tempo = 5 * 60 * 60000; break;
+		case 4: tempo = 5 * 60 * 60000; break;
+	}
+	
+	var date = new Date();
+
+	return date.getTime() + tempo; 
+}
+
 
 function getJogoDAO(application){
 	var connection = application.config.dbConnection;
